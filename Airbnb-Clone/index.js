fdf56a3841455991b20fcdabfc352cc6a32be0d2
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-09-03 14:30:14 +0530

    Add files via upload

---
+++ Airbnb-Clone/index.js
@@ -0,0 +1,146 @@
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config();
+}
+// console.log(process.env.SECRET);
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+// const Server = require("./server.js");
+
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+const Listing = require("./models/listing.js");
+const Users = require("./models/user.js");
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+main()
+    .then(() => {
+        console.log("connected to db");
+    }).catch((err) => {
+        console.log("Database connection error: ", err);
+    });
+
+async function main() {
+    await mongoose.connect(dbUrl
+    );
+}
+
+//mongostore
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: "mysupersecretecode"
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", ()=>{
+    console.log("ERROR in MONGO SESSION STORE: ", err);
+})
+
+//function with session option
+const sessionOptions = {
+    store,
+    secret: "mysupersecretecode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+    }
+}
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(cors());
+app.engine('ejs', ejsMate);
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    // console.log(res.locals.success);
+    // res.locals.currUser = req.user;
+    next();
+});
+
+app.use((req, res, next) => {
+    res.locals.currUser = req.user || null;
+    console.log(res.locals.currUser);
+    next();
+  });
+
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student",
+    });
+
+    let rgUser = await User.register(fakeUser, "helloworld");
+    res.send(rgUser);
+
+});
+
+
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
+
+app.get("/listings/:id/book",async(req,res) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    let name = await Users.findOne({ _id: listing.owner._id});
+    console.log(name.username);
+    console.log(listing.title);
+    console.log(res.locals.currUser.username);
+    res.redirect("/listings");
+});
+
+app.use("/", (req, res) => {
+    res.redirect("/listings")
+})
+
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong" } = err;
+
+    res.status(statusCode).render("error.ejs", { err });
+    // res.send("Something went wrong");
+    // res.status(statusCode).send(message);
+});
+
+app.listen(8000, () => {
+    console.log("server is listening on port 8000");
+});
